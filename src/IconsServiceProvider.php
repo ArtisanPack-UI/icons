ad733696011e5323379862bc322d5581e535229c
Jacob Martella <me@jacobmartella.com>
2025-04-21 21:35:19 -0500

    Continued adding in the free font awesome icons.
    Addresses #2

--- src/IconsServiceProvider.php
+++ src/IconsServiceProvider.php
@@ -18,11 +18,19 @@ class IconsServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishes( [
-            __DIR__ . '../dist/css/all.css' => public_path( 'vendor/digitalshopfront/icons' ),
+            __DIR__ . '/../dist/css/all.css'                       => public_path( 'vendor/digitalshopfront/icons/css/all.css' ),
+            __DIR__ . '/../dist/webfonts/fa-brands-400.ttf'        => public_path( 'vendor/digitalshopfront/icons/webfonts/fa-brands-400.ttf' ),
+            __DIR__ . '/../dist/webfonts/fa-brands-400.woff2'      => public_path( 'vendor/digitalshopfront/icons/webfonts/fa-brands-400.woff2' ),
+            __DIR__ . '/../dist/webfonts/fa-regular-400.ttf'       => public_path( 'vendor/digitalshopfront/icons/webfonts/fa-regular-400.ttf' ),
+            __DIR__ . '/../dist/webfonts/fa-regular-400.woff2'     => public_path( 'vendor/digitalshopfront/icons/webfonts/fa-regular-400.woff2' ),
+            __DIR__ . '/../dist/webfonts/fa-solid-900.ttf'         => public_path( 'vendor/digitalshopfront/icons/webfonts/fa-solid-400.ttf' ),
+            __DIR__ . '/../dist/webfonts/fa-solid-900.woff2'       => public_path( 'vendor/digitalshopfront/icons/webfonts/fa-solid-900.woff2' ),
+            __DIR__ . '/../dist/webfonts/fa-v4compatibility.ttf'   => public_path( 'vendor/digitalshopfront/icons/webfonts/fa-v4compatibility.ttf' ),
+            __DIR__ . '/../dist/webfonts/fa-v4compatibility.woff2' => public_path( 'vendor/digitalshopfront/icons/webfonts/fa-v4compatibility.woff2' ),
         ], 'public' );
 
         Blade::directive( 'dsIcons', function ( $expression ) {
-            return '<link href="' . asset( 'vendor/digitalshopfront/icons' ) . '" rel="stylesheet">';
+            return '<link href="' . asset( 'vendor/digitalshopfront/icons/css/all.css' ) . '" rel="stylesheet">';
         } );
     }
 }
