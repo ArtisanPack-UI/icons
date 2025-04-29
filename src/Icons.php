d7a365dd4f71f9b04124ffb5edd58c892fbad395
Jacob Martella <me@jacobmartella.com>
2025-04-29 17:48:31 -0500

    Added in the filter functionality for getting icons.

--- src/Icons.php
+++ src/Icons.php
@@ -7,18 +7,26 @@ use TorMorten\Eventy\Facades\Eventy;
 class Icons
 {
 
-    public function getIcons( $args = [] ): array
-    {
-        $icons = $this->iconsList();
+	public function getIcons( $args = [] ): array
+	{
+		$icons = $this->iconsList();
 
-        return $icons;
-    }
+		if ( !empty( $args ) ) {
+			foreach ( $args as $key => $arg ) {
+				$icons = array_filter( $icons, function ( $icon ) use ( $arg, $key ) {
+					return in_array( $icon[ $key ], $arg ) || $icon[ $key ] === $arg;
+				} );
+			}
+		}
 
-    public function iconsList(): array
-    {
-        $icons = [];
+		return $icons;
+	}
 
-        return Eventy::filter( 'ds.icons.iconsList', $icons );
-    }
+	public function iconsList(): array
+	{
+		$icons = [];
+
+		return Eventy::filter( 'ds.icons.iconsList', $icons );
+	}
 
 }
